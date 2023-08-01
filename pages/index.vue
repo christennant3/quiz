e1d31baed1977aa71810c79718635a508f0b4f3b
Chris Tennant <chris>
2023-08-01 17:17:07 +0100

    bare bones quiz working

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,45 @@
 <template>
   <div class="max-w-3xl mx-auto bg-slate-50 p-6 rounded-lg shadow-lg">
     <h1>Quiz</h1>
+    <p>Question {{ questionIndex + 1  }} / {{ questions.length }}</p>
+    <p class="pb-8">Score: {{ score }} / {{ questions.length }}</p>
+    <div v-if="visible.question">
+      
+      <div v-for="question, index in questions">
+        <template v-if="questionIndex === index">
+          <p class="text-2xl pb-4">{{ question.text }}</p>
 
-    <div v-for="question, index in questions">
-      <template v-if="questionIndex === index">
-        <p class="text-2xl pb-4">{{ question.text }}</p>
+          <div v-for="(option, index) in question.options">
 
-        <div v-for="(option, index) in question.options">
-          
-            <button class="p-3 bg-stone-300 rounded w-full m-2" @click="answer(option, index)">
-            {{ option }}
+            <button class="p-3 bg-stone-300 rounded w-full m-2 hover:bg-stone-400" @click="answer(option, index)" :disabled="isDisabled" :class="{ 'btn-disabled': isDisabled }">
+              {{ option }}
             </button>
 
-          
-        </div>
-      </template>
+          </div>
+        </template>
 
 
+      </div>
     </div>
 
     <div v-show="visible.feedback">
 
-            <p v-show="visible.correct" class="text-green-600 font-bold text-xl pl-8 m-2">Correct!</p>
-            <p v-show="visible.wrong" class="text-red-600 font-bold text-xl pl-8 m-2">Wrong!</p>
-          
-            <p>Score: {{ score }}</p>
+      <p v-show="visible.correct" class="text-green-600 font-bold text-3xl pl-2 m-2">Correct!</p>
+      <p v-show="visible.wrong" class="text-red-600 font-bold text-3xl pl-2 m-2">Wrong!</p>
+    
+    </div>
 
-          </div>
+    <div v-show="visible.result">
+      <p class="font-bold text-3xl pl-8 m-2">You got {{ score }} out of {{ questions.length }}</p>
+    </div>
 
     <div class="flex justify-center">
       <button @click="nextBtn" v-show="visible.next"
         class="bg-green-600 p-2 rounded m-2 text-white w-1/2 hover:bg-green-700 mt-6 w-50 text-center">Next</button>
 
     </div>
+    <p>Question {{ questionIndex + 1 }} / {{ questions.length }}</p>
+    <p>Score: {{ score }} / {{ questions.length }}</p>
   </div>
 </template>
 
@@ -41,13 +48,15 @@ import { useAuthStore } from '~/stores/useAuthStore'
 const auth = useAuthStore();
 const questionIndex = ref(0);
 const score = ref(0);
+const isDisabled = ref(false);
 const visible = reactive([
   {
     next: false,
     feedback: false,
     result: false,
     correct: false,
-    wrong: false
+    wrong: false,
+    question: true
   }
 ]);
 const questions = reactive([
@@ -74,34 +83,56 @@ const questions = reactive([
 ])
 
 onMounted(() => {
+  visible.question = true;
   questions.sort(() => Math.random() - 0.5)
 })
 
 function answer(option, index) {
-  
-  if (index === questions[questionIndex.value].correctIndex) { 
+
+  if (index === questions[questionIndex.value].correctIndex) {
     // correct answer 
     score.value++
     visible.correct = true;
   } else {
     // wrong answer
-   visible.wrong = true;
+    visible.wrong = true;
   }
   visible.next = true;
   visible.feedback = true;
-  
+  isDisabled.value = true;
+
 }
 
 function nextBtn() {
   visible.next = false;
   visible.correct = false;
   visible.wrong = false;
+  isDisabled.value = false;
+
   questionIndex.value++
+
+  if (questionIndex.value === questions.length) {
+    visible.result = true;
+    visible.next = false;
+    visible.correct = false;
+    visible.wrong = false;
+    isDisabled.value = false;
+
+  }
 }
 
-
+const totalQuestions = computed(() => questions.length);
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  button {
+    cursor: pointer;
+  }
+
+  .btn-disabled {
+    cursor: not-allowed;
+  }
+
+</style>
 
