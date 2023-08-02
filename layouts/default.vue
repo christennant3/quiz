9143746f24360a7687c720b1b32297727e7bc6cd
Chris Tennant <chris>
2023-08-02 10:29:08 +0100

    remove login

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
             <ul class="flex gap-4">
                 <li><router-link to="/">Home</router-link></li>
                 <li><router-link to="/about">About</router-link></li>
-                <li><router-link to="/login">Login</router-link></li>
+                <!-- <li><router-link to="/login">Login</router-link></li> -->
             </ul>
         </nav>
     </header>
